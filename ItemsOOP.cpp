0a443adcfc6ec32e5cc8ec2a245d235cb4ef286b
Viktor Lychkatyi <137647165+ViktorLychkatyi@users.noreply.github.com>
2024-10-25 14:35:56 +0200

    second commit

--- ItemsOOP.cpp
+++ ItemsOOP.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 
 // Телефон
-// класс
 class Phone {
-public:
-	// публичные поля
+private:
 	string brand;						// Бренд
 	string model;						// Модель
 	string memory_size;					// Память устройства
@@ -13,7 +11,65 @@ public:
 	string operating_system;			// Операционная система
 	string proccesor;					// Процессор
 
-	// публичные классы
+public:
+	void SetBrand(string b) {
+		brand = b;
+	}
+
+	string GetBrand(string b) {
+		return brand;
+	}
+
+	void SetModel(string m) {
+		model = m;
+	}
+
+	string GetModel(string m) {
+		return m;
+	}
+
+	void SetMemorySize(string s) {
+		memory_size = s;
+	}
+
+	string GetMemorySize(string s) {
+		return memory_size;
+	}
+
+	void SetColor(string c) {
+		color = c;
+	}
+
+	string GetColor(string c) {
+		return color;
+	}
+
+	void SetOperatingSystem(string o) {
+		operating_system = o;
+	}
+
+	string GetOperatingSystem(string o) {
+		return operating_system;
+	}
+
+	void SetProcessor(string p) {
+		proccesor = p;
+	}
+
+	string GetProcessor(string p) {
+		return proccesor;
+	}
+
+	void PrintPhone() {
+		cout << brand << "\n";
+		cout << model << "\n";
+		cout << memory_size << "\n";
+		cout << color << "\n";
+		cout << operating_system << "\n";
+		cout << proccesor << "\n";
+		cout << "\n";
+	}
+
 	void Call() {						// Звонить
 		cout << "Звоним в телефон...\n";
 	}
@@ -40,17 +96,64 @@ public:
 };
 
 // Купюра
-// класс
 class Banknote {
-public:
-	// публичные поля
+private:
 	string denomination;				// Номинация
 	string currency;					// Валюта
 	string serial_number;				// Серийный номер
 	string country;						// Страна
 	string state;						// Состояние
 
-	// публичные классы
+public:
+	void SetDenomination(string d) {
+		denomination = d;
+	}
+
+	string GetDenomination(string b) {
+		return denomination;
+	}
+
+	void SetCurrency(string c) {
+		currency = c;
+	}
+
+	string GetCurrency(string c) {
+		return c;
+	}
+
+	void SetSerialNumber(string s) {
+		serial_number = s;
+	}
+
+	string GetSerialNumber(string s) {
+		return serial_number;
+	}
+
+	void SetCountry(string c) {
+		country = c;
+	}
+
+	string GetCountry(string c) {
+		return country;
+	}
+
+	void SetState(string s) {
+		state = s;
+	}
+
+	string GetState(string s) {
+		return state;
+	}
+
+	void PrintBanknote() {
+		cout << denomination << "\n";
+		cout << currency << "\n";
+		cout << serial_number << "\n";
+		cout << country << "\n";
+		cout << state << "\n";
+		cout << "\n";
+	}
+
 	void Exchange() {					// Обменять
 		cout << "Вы обменяли доллар на евро\n";
 	}
@@ -77,17 +180,64 @@ public:
 };
 
 // Электрочайник
-// класс
 class ElectricKettle {
-public:
-	// публичные поля
+private:
 	string сapacity;					// Объем
 	string power;						// Мощность
 	string body_material;				// Материал корпуса
 	string color;						// Цвет
 	string water_temperature;			// Текущая температура воды
 
-	// публичные классы
+public:
+	void SetCapacity(string c) {
+		сapacity = c;
+	}
+
+	string GetCapacity(string c) {
+		return сapacity;
+	}
+
+	void SetPower(string p) {
+		power = p;
+	}
+
+	string GetPower(string p) {
+		return power;
+	}
+
+	void SetBodyMaterial(string m) {
+		body_material = m;
+	}
+
+	string GetBodyMaterial(string m) {
+		return body_material;
+	}
+
+	void SetColor(string c) {
+		color = c;
+	}
+
+	string GetColor(string c) {
+		return color;
+	}
+
+	void SetWaterTemperature(string t) {
+		water_temperature = t;
+	}
+
+	string GetWaterTemperature(string t) {
+		return water_temperature;
+	}
+
+	void PrintElectricKettle() {
+		cout << сapacity << "\n";
+		cout << power << "\n";
+		cout << body_material << "\n";
+		cout << color << "\n";
+		cout << water_temperature << "\n";
+		cout << "\n";
+	}
+
 	void TurnOn() {						// Включить чайник
 		cout << "Включаем чайник\n";
 		cout << "На дисплее загорается свет, и через мгновение слышен шипящий звук, когда вода начинает нагреваться.\n";
@@ -112,17 +262,64 @@ public:
 };
 
 // Гелевая ручка
-// класс
 class GelPen {
-public:
-	// публичные поля
+private:
 	string ink_color;					// Цвет чернил
 	string pen_thickness;				// Толщина стержня
 	string pen_length;					// Длина стержня
 	string body_material;				// Материал корпуса
 	string ink_amount;					// Количество оставшихся чернил
 
-	// публичные классы
+public:
+	void SetInkColor(string i) {
+		ink_color = i;
+	}
+
+	string GetInkColor(string i) {
+		return ink_color;
+	}
+
+	void SetPenThickness(string t) {
+		pen_thickness = t;
+	}
+
+	string GetPenThickness(string t) {
+		return pen_thickness;
+	}
+
+	void SetPenLength(string l) {
+		pen_length = l;
+	}
+
+	string GetPenLength(string l) {
+		return pen_length;
+	}
+
+	void SetBodyMaterial(string m) {
+		body_material = m;
+	}
+
+	string GetBodyMaterial(string m) {
+		return body_material;
+	}
+
+	void SetInkAmount(string a) {
+		ink_amount = a;
+	}
+
+	string GetInkAmount(string a) {
+		return ink_amount;
+	}
+
+	void PrintGelPen() {
+		cout << ink_color << "\n";
+		cout << pen_thickness << "\n";
+		cout << pen_length << "\n";
+		cout << body_material << "\n";
+		cout << ink_amount << "\n";
+		cout << "\n";
+	}
+
 	void Write() {						// Написать
 		cout << "Пишем в тетрадь\n";
 	}
@@ -145,17 +342,64 @@ public:
 };
 
 // Книга
-// класс
 class Book {
-public:
-	// публичные поля
+private:
 	string title;						// Название
 	string author;						// Автор
 	int number_pages;					// Количество страниц
 	string genre;						// Жанр
 	int year_publication;				// Год издания
 
-	// публичные классы
+public:
+	void SetTitle(string t) {
+		title = t;
+	}
+
+	string GetTitle(string t) {
+		return title;
+	}
+
+	void SetAuthor(string a) {
+		author = a;
+	}
+
+	string GetAuthor(string a) {
+		return author;
+	}
+
+	void SetNumberPages(int n) {
+		number_pages = n;
+	}
+
+	int GetNumberPages(int n) {
+		return number_pages;
+	}
+
+	void SetGenre(string g) {
+		genre = g;
+	}
+
+	string GetGenre(string g) {
+		return genre;
+	}
+
+	void SetYearPublication(int y) {
+		year_publication = y;
+	}
+
+	int GetYearPublication(int y) {
+		return year_publication;
+	}
+
+	void PrintBook() {
+		cout << title << "\n";
+		cout << author << "\n";
+		cout << number_pages << "\n";
+		cout << genre << "\n";
+		cout << year_publication << "\n";
+		cout << "\n";
+	}
+
 	void Open() {						// Открыть книгу
 		cout << "Вы открыли книгу\n";
 	}
@@ -181,7 +425,7 @@ public:
 	}
 
 	void NextPage() {					// Следующая страница книги
-		cout << "Вы открываете следующую страницу";
+		cout << "Вы открываете следующую страницу\n";
 	}
 
 	void PrevPage() {					// Предыдущая страница книги
@@ -193,14 +437,14 @@ public:
 int main() {
 	setlocale(0, "");
 
-	// Телефон 
 	Phone phone;
-	phone.brand = "Samsung";
-	phone.model = "Galaxy A55";
-	phone.memory_size = "256 GB";
-	phone.color = "Black";
-	phone.operating_system = "Android";
-	phone.proccesor = "Exynos 1480";
+	phone.SetBrand("Samsung");
+	phone.SetModel("Galaxy A55");
+	phone.SetMemorySize("256 GB");
+	phone.SetColor("Black");
+	phone.SetOperatingSystem("Android");
+	phone.SetProcessor("Exynos 1480");
+	phone.PrintPhone();
 
 	phone.Call();
 	phone.SendMessage();
@@ -210,13 +454,13 @@ int main() {
 	phone.Charge();
 	cout << "\n";
 
-	// Купюра
 	Banknote banknote;
-	banknote.denomination = "The United of States";
-	banknote.currency = "100 USD";
-	banknote.serial_number = "KB46279860IB2";
-	banknote.country = "USA";
-	banknote.state = "New";
+	banknote.SetDenomination("The United of States");
+	banknote.SetCurrency("100 USD");
+	banknote.SetSerialNumber("KB46279860IB2");
+	banknote.SetCountry("USA");
+	banknote.SetState("New");
+	banknote.PrintBanknote();
 
 	banknote.Exchange();
 	banknote.State();
@@ -225,13 +469,13 @@ int main() {
 	banknote.Touch();
 	cout << "\n";
 
-	// Электрочайник
 	ElectricKettle electric_kettle;
-	electric_kettle.сapacity = "1.7 литра";
-	electric_kettle.power = "2000 Вт";
-	electric_kettle.body_material = "Нержавеющая сталь";
-	electric_kettle.color = "Черный";
-	electric_kettle.water_temperature = "25°C";
+	electric_kettle.SetCapacity("1.7 литра");
+	electric_kettle.SetPower("2000 Вт");
+	electric_kettle.SetBodyMaterial("Нержавеющая сталь");
+	electric_kettle.SetColor("Черный");
+	electric_kettle.SetWaterTemperature("25°C");
+	electric_kettle.PrintElectricKettle();
 
 	electric_kettle.TurnOn();
 	electric_kettle.TurnOff();
@@ -240,13 +484,13 @@ int main() {
 	electric_kettle.AutoOffKettle();
 	cout << "\n";
 
-	// Гелевая ручка
 	GelPen gel_pen;
-	gel_pen.ink_color = "Синий";
-	gel_pen.pen_thickness = "0.5 мм";
-	gel_pen.pen_length = "14 см";
-	gel_pen.body_material = "Пластик";
-	gel_pen.ink_amount = "50%";
+	gel_pen.SetInkColor("Синий");
+	gel_pen.SetPenThickness("0.5 мм");
+	gel_pen.SetPenLength("14 см");
+	gel_pen.SetBodyMaterial("Пластик");
+	gel_pen.SetInkAmount("50%");
+	gel_pen.PrintGelPen();
 
 	gel_pen.Write();
 	gel_pen.ChangeAmpoule();
@@ -255,13 +499,13 @@ int main() {
 	gel_pen.OpenCap();
 	cout << "\n";
 
-	// Книга
 	Book book;
-	book.title = "1984";
-	book.author = "Джордж Оруэлл";
-	book.number_pages = 328;
-	book.genre = "Антиутопия";
-	book.year_publication = 1949;
+	book.SetTitle("1984");
+	book.SetAuthor("Джордж Оруэлл");
+	book.SetNumberPages(328);
+	book.SetGenre("Антиутопия");
+	book.SetYearPublication(1949);
+	book.PrintBook();
 
 	book.Open();
 	book.Close();
